c126afdf3c1a115e00be328b0e4af0dde945cbb8
rosie98 <106025529+rosieisor@users.noreply.github.com>
2025-08-11 22:34:35 +0900

    Time: 0 ms (100%), Space: 41.4 MB (64.39%) - LeetHub

--- 0225-implement-stack-using-queues/0225-implement-stack-using-queues.java
+++ 0225-implement-stack-using-queues/0225-implement-stack-using-queues.java
@@ -2,26 +2,60 @@ import java.util.*;
 
 class MyStack {
 
-    Deque<Integer> deque;
+    // 1. Deque 사용
+    // Deque<Integer> deque;
+
+    // 2. 두 개의 Queue 사용
+    Queue<Integer> q1;      // 큐
+    Queue<Integer> q2;      // 리버스 큐
 
     public MyStack() {
-        deque = new LinkedList<>();
+        // deque = new LinkedList<>();
+
+        q1 = new LinkedList<>();
+        q2 = new LinkedList<>();
     }
     
     public void push(int x) {
-        deque.offer(x);
+        // deque.offer(x);
+
+        q1.offer(x);
     }
     
     public int pop() {
-        return deque.pollLast();
+        // return deque.pollLast();
+
+        System.out.println(q1.size());
+        while(q1.size() > 1) {
+            q2.offer(q1.poll());
+        }
+
+        int data = q1.poll();
+        q1 = q2;
+        q2 = new LinkedList<>();
+
+        return data;
     }
     
     public int top() {
-        return deque.getLast();
+        // return deque.getLast();
+
+        while(q1.size() > 1) {
+            q2.offer(q1.poll());
+        }
+
+        int data = q1.poll();
+        q2.offer(data);
+        q1 = q2;
+        q2 = new LinkedList<>();
+
+        return data;
     }
     
     public boolean empty() {
-        return deque.isEmpty();
+        // return deque.isEmpty();
+
+        return q1.isEmpty();
     }
 }
 
