adba196cf8e13e32a11302367a40dc55af7ce3fd
rosie98 <106025529+rosieisor@users.noreply.github.com>
2025-08-27 17:24:56 +0900

    Time: 5 ms (28.34%), Space: 42.4 MB (26.8%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
@@ -19,17 +19,13 @@ class Solution {
     public void backTracking(int pick, String making, String digits) {
         if (pick == digits.length()) {
             result.add(making);
-            // System.out.println(making);
             return;
         }
 
         String letters = map.get(Integer.parseInt(digits.substring(pick, pick+1)));
-        // System.out.println(letters);
 
         for(int i=0; i < letters.length(); i++) {
-            System.out.println(i + making);
             backTracking(pick + 1, making + letters.charAt(i), digits);
-            System.out.println(making);
         }
     }
 
