33d4e8103e369a277a51154759be81bdf5c01221
rosie98 <106025529+rosieisor@users.noreply.github.com>
2025-08-31 00:04:59 +0900

    Time: 32 ms (37.95%), Space: 55.4 MB (20.32%) - LeetHub

--- 0817-design-hashmap/0817-design-hashmap.java
+++ 0817-design-hashmap/0817-design-hashmap.java
@@ -1,24 +1,21 @@
 class MyHashMap {
-    Map<Integer, Integer> hashMap;
+    int[] arr;
 
     public MyHashMap() {
-        hashMap = new HashMap<>();
+        arr = new int[1000001];
+        Arrays.fill(arr, -1);
     }
     
     public void put(int key, int value) {
-        hashMap.put(key, value);
+        arr[key] = value;
     }
     
     public int get(int key) {
-        if (hashMap.get(key) == null) {
-            return -1;
-        }
-
-        return hashMap.get(key);
+        return arr[key];
     }
     
     public void remove(int key) {
-        hashMap.remove(key);
+        arr[key] = -1;
     }
 }
 
