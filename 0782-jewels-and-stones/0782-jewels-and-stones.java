8bf3b6ab34a0b30bfcae7ed19aeee3628b57ebc6
rosie98 <106025529+rosieisor@users.noreply.github.com>
2025-08-31 00:25:05 +0900

    Time: 2 ms (11.54%), Space: 42.2 MB (16.08%) - LeetHub

--- 0782-jewels-and-stones/0782-jewels-and-stones.java
+++ 0782-jewels-and-stones/0782-jewels-and-stones.java
@@ -1,15 +1,20 @@
 class Solution {
     public int numJewelsInStones(String jewels, String stones) {
-        int result = 0;
+        Map<Character, Integer> map = new HashMap<>();
 
-        String[] arr = stones.split("");
-
-        for(int i=0; i<arr.length; i++) {
-            if (jewels.contains(arr[i])){
-                result++;
+        for(int i=0; i<stones.length(); i++) {
+            if (map.get(stones.charAt(i)) == null) {
+                map.put(stones.charAt(i), 1);
+            } else {
+                map.put(stones.charAt(i), map.get(stones.charAt(i)) + 1);
             }
         }
-        
+
+        int result = 0;
+        for(int i=0; i<jewels.length(); i++) {
+            result += (map.get(jewels.charAt(i)) == null) ? 0 : map.get(jewels.charAt(i));
+        }
+
         return result;
     }
 }
