398e526a7241aa1976a72f508dee2330bfc87e13
rosie98 <106025529+rosieisor@users.noreply.github.com>
2025-09-01 22:06:54 +0900

    Time: 28 ms (74.88%), Space: 56.5 MB (98.18%) - LeetHub

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.java
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.java
@@ -1,17 +1,49 @@
 class Solution {
     public int findKthLargest(int[] nums, int k) {
-        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+        quickSort(0, nums.length-1, nums);
 
-        for(int num : nums) {
-            pq.add(num);
+        return nums[nums.length-k];
+    }
+
+    public void quickSort(int left, int right, int[] nums) {
+        if (left < right) {
+            int mid = partition(left, right, nums);
+
+            quickSort(left, mid, nums);
+            quickSort(mid + 1, right, nums);
+        }
+    }
+
+    public int partition(int left, int right, int[] nums) {
+        int low = left;
+        int high = right;
+
+        int pivot = nums[(left+right)/2];
+
+        while(true) {
+            while(nums[low] < pivot) {
+                low++;
+            }
+
+            while(nums[high] > pivot) {
+                high--;
+            }
+
+            if (low >= high) {
+                return high;
+            }
+
+            swap(low, high, nums);
+
+            low++;
+            high--;
         }
 
-        int index = 0;
-        while (index < k - 1) {
-            pq.poll();
-            index++;
-        }
+    }
 
-        return pq.poll();
+    public void swap(int low, int right, int[] nums) {
+        int temp = nums[low];
+        nums[low] = nums[right];
+        nums[right] = temp;
     }
 }
